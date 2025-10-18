da32cccd277efa0dc3450335759597b8064f8d20
Zehua Chen <nigga@gmail.com>
2025-10-18 20:40:48 +0800

    fuck fly.io lah

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
-    port: 5173,
+    port: 8080,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
